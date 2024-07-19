5bebb45b18716a2ceb45a3b90f8b4ae498849258
Jonathan Anzules <jonanzule@gmail.com>
2024-07-19 09:52:10 -0700

    Integrated

---
+++ LargeScale/matrix_analysis/StandardWorkflow_solo.R
@@ -0,0 +1,216 @@
+library(Seurat)
+library(ggplot2)
+library(SeuratDisk)
+library(SeuratData)
+
+T2D <- readRDS("C://Users/jonan/Documents/1Work/scWork/Beta_Cell_Study/Data/LargeScale/count_matrix_T2D.rds")
+healthy <- readRDS("C://Users/jonan/Documents/1Work/scWork/Beta_Cell_Study/Data/LargeScale/count_matrix_healthy.rds")
+
+# Set up
+output_fig <- "C://Users/jonan/Documents/1Work/scWork/Beta_Cell_Study/Figures/SecondSet/"
+
+#----------------------------------Cleaning------------------------------------
+T2D[["percent.mt"]] <- PercentageFeatureSet(T2D, pattern = "^MT-")
+healthy[["percent.mt"]] <- PercentageFeatureSet(healthy, pattern = "^MT-")
+
+qc_output_t2d <- paste0(output_fig, "QC_violin_t2d.png")
+qc_output_healthy <- paste0(output_fig, "QC_violin_healthy.png")
+
+# Visualize QC metrics as a violin plot - t2d
+png(filename = qc_output_t2d, width = 1000, height = 800)
+VlnPlot(T2D, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+dev.off()
+
+# Visualize QC metrics as a violin plot - healthy
+png(filename = qc_output_healthy, width = 1000, height = 800)
+VlnPlot(healthy, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+dev.off()
+
+T2D <- subset(T2D, subset = nFeature_RNA > 1000 & nFeature_RNA < 9000 & nCount_RNA < 1850000 & percent.mt < 78)
+# dim(T2D) Original - 42148  3862
+# dim(T2D) New      - 42148  3435
+healthy <- subset(healthy, subset = nFeature_RNA > 500 & nFeature_RNA < 9000 & nCount_RNA < 1850000 & percent.mt < 78)
+# dim(healthy) Original - 42148  7341
+# dim(healthy) New      - 42148  5868
+
+FeatureScatter(T2D, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")+
+  geom_smooth(method='lm') +
+  geom_hline(yintercept = 1850)+
+  geom_vline(xintercept = 300000)
+
+FeatureScatter(healthy, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")+
+  geom_smooth(method='lm')
+
+
+#--------------------------Standard Processing -------------------------------
+
+
+# Normalizing
+T2D <- NormalizeData(T2D)
+healthy <- NormalizeData(healthy)
+
+# Highly variable genes
+T2D <- FindVariableFeatures(T2D, selection.method = "vst", nfeatures = 2000)
+healthy <- FindVariableFeatures(healthy, selection.method = "vst", nfeatures = 2000)
+
+# Top 10 of both
+top10_T2D <- head(VariableFeatures(T2D), 10)
+top10_healthy <- head(VariableFeatures(healthy), 10)
+
+### Figures - Highly variable genes
+highly_variable_t2d <- paste0(output_fig, "highly_variable_t2d.png")
+highly_variable_healthy <- paste0(output_fig, "highly_variable_healthy.png")
+
+# T2D
+png(filename = highly_variable_t2d, width = 1000, height = 800)
+variable_t2d <- VariableFeaturePlot(T2D)
+LabelPoints(plot = variable_t2d, points = top10_T2D)
+dev.off()
+
+# healthy
+png(filename = highly_variable_healthy, width = 1000, height = 800)
+variable_healthy <- VariableFeaturePlot(healthy)
+LabelPoints(plot = variable_healthy, points = top10_healthy)
+dev.off()
+
+#---------------------Accounting for variations-------------------------------
+# Cell cycles and batch effects
+
+all.genes_T2D <- rownames(T2D)
+T2D <- ScaleData(T2D, features = all.genes_T2D)
+
+all.genes_healthy <- rownames(healthy)
+healthy <- ScaleData(healthy, features = all.genes_healthy)
+
+#--------------------Linear Dimensionality reduction PCA------------------------
+#Identify the sources of heterogeneity in the dataset
+
+T2D <- RunPCA(T2D, features = VariableFeatures(object = T2D))
+healthy <- RunPCA(healthy, features = VariableFeatures(object = healthy))
+
+# dims = first 5 PCA but only the first 5 (nfeatures) features
+print(T2D[["pca"]], dims=1:5, nfeatures = 5)
+DimHeatmap(T2D, dims = 1, cells = 500, balanced = TRUE)
+
+# Elbow plot
+# Choosing only those statistically significant components that captures the
+# majority of the signal in our downstream analysis
+#Ranked by the percentage of the variance explained
+
+ElbowPlot(T2D) #12
+ElbowPlot(healthy) #12
+
+# These could adversely effect the downstream analysis
+
+
+#----------------------Clustering based on PCA----------------------------------
+T2D <- FindNeighbors(T2D, dims = 1:12)
+healthy <- FindNeighbors(healthy, dims = 1:12)
+
+# Checking out the resolutions
+T2D <- FindClusters(T2D, resolution = c(0.1, 0.3, 0.5, 0.7, 1))
+View(T2D@meta.data$RNA_snn_res.0.1)
+healthy <- FindClusters(healthy, resolution = c(0.1, 0.3, 0.5, 0.7, 1))
+
+DimPlot(T2D, group.by = "RNA_snn_res.0.5", label = TRUE)
+DimPlot(healthy, group.by = "RNA_snn_res.0.7", label = TRUE)
+
+# Setting identify of clusters
+Idents(T2D) <- "RNA_snn_res.0.5"
+Idents(healthy) <- "RNA_snn_res.0.7"
+
+#--------------------------------UMAP-------------------------------------------
+# reticulate::py_install(packages = 'umap-learn')
+library(umap)
+
+T2D <- RunUMAP(T2D, dims = 1:12)
+healthy <- RunUMAP(healthy, dims = 1:12)
+
+DimPlot(T2D, reduction = "umap")
+DimPlot(T2D, reduction = "umap", group.by = "patient_name")
+
+DimPlot(healthy, reduction = "umap")
+DimPlot(healthy, reduction = "umap", group.by = "patient_name")
+
+unique(T2D@meta.data$patient_name)
+
+
+#-----------------------------Saving-------------------------------------------
+SaveH5Seurat(object = T2D,
+              filename = "C://Users/jonan/Documents/1Work/scWork/Beta_Cell_Study/Data/LargeScale/T2D_clean_clustered") 
+
+
+#--------------------------Testing Merging -------------------------------------
+
+# Merge Seurat objects
+merged_object <- merge(seurat_object1, y = seurat_object2, 
+                       add.cell.ids = c("Set1", "Set2"), 
+                       project = "Merged_Dataset")
+
+
+
+
+
+
+
+
+
+
+
+ #
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------Doublet Finder Place holder------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ LargeScale/matrix_analysis/integrating_T2D_healthy.R
@@ -0,0 +1,135 @@
+library(Seurat)
+library(ggplot2)
+library(SeuratDisk)
+library(SeuratData)
+library(tidyverse)
+library(gridExtra)
+library(grid)
+
+T2D <- readRDS("C://Users/jonan/Documents/1Work/scWork/Beta_Cell_Study/Data/LargeScale/count_matrix_T2D.rds")
+healthy <- readRDS("C://Users/jonan/Documents/1Work/scWork/Beta_Cell_Study/Data/LargeScale/count_matrix_healthy.rds")
+T2D$condition <- 'T2D'
+healthy$condition <- 'Healthy'
+
+#------------------------Quality Control n Merging------------------------------
+T2D[["percent.mt"]] <- PercentageFeatureSet(T2D, pattern = "^MT-")
+healthy[["percent.mt"]] <- PercentageFeatureSet(healthy, pattern = "^MT-")
+
+T2D <- subset(T2D, subset = nFeature_RNA > 1000 & nFeature_RNA < 9000 & nCount_RNA < 1850000 & percent.mt < 78)
+# dim(T2D) Original - 42148  3862
+# dim(T2D) New      - 42148  3435
+healthy <- subset(healthy, subset = nFeature_RNA > 500 & nFeature_RNA < 9000 & nCount_RNA < 1850000 & percent.mt < 78)
+# dim(healthy) Original - 42148  7341
+# dim(healthy) New      - 42148  5868
+
+# Merge the two Seurat objects with an additional layer of identifiers
+Islets_merged <- merge(T2D, y = healthy, add.cell.ids = c("T2D", "Healthy"), 
+                       project = "Combined_Islets")
+
+#------------------------Standard Workflow-------------------------------------
+
+Islets_merged <- NormalizeData(Islets_merged)
+Islets_merged <- FindVariableFeatures(Islets_merged)
+Islets_merged <- ScaleData(Islets_merged)
+Islets_merged <- RunPCA(Islets_merged)
+ElbowPlot(Islets_merged)
+Islets_merged <- FindNeighbors(Islets_merged, dims = 1:20)
+Islets_merged <- FindClusters(Islets_merged)
+Islets_merged <- RunUMAP(Islets_merged, dims = 1:20)
+
+
+
+#-------------------------Visualizing batch effect------------------------------
+
+head(Islets_merged@meta.data)
+
+p1 <- DimPlot(Islets_merged, reduction = "umap")
+p2 <- DimPlot(Islets_merged, reduction = 'umap', group.by = "patient_name")
+p3 <- DimPlot(Islets_merged, reduction = 'umap', group.by = "condition")
+
+combined_plot <- grid.arrange(p1, p2, p3, ncol = 2, nrow = 2)
+
+png("C://Users/jonan/Documents/1Work/scWork/Beta_Cell_Study/Figures/SecondSet/before_integration.png",
+    width = 2000, height = 1200)
+
+grid.draw(combined_plot)
+dev.off()
+
+#Signs of technical variation causes clustering, not biological variation
+
+
+
+#--------------------Performing integration------------------------------------
+obj.list <- SplitObject(Islets_merged, split.by = 'patient_name')
+
+for( i in 1:length(obj.list)){
+  obj.list[[i]] <- NormalizeData(object = obj.list[[i]])
+  obj.list[[i]] <- FindVariableFeatures(object = obj.list[[i]])
+}
+
+# Selecting Integration features
+features <- SelectIntegrationFeatures(object.list = obj.list)
+
+# Find integration anchors (CCA)
+anchors <- FindIntegrationAnchors(object.list = obj.list,
+                       anchor.features = features, k.filter = )
+
+# Integrate data
+islet_integrated <- IntegrateData(anchorset = anchors)
+
+
+#-------------------Standard Workflow -----------------------------------------
+islet_integrated <- ScaleData(object = islet_integrated)
+islet_integrated <- RunPCA(object = islet_integrated)
+ElbowPlot(islet_integrated)
+islet_integrated <- FindNeighbors(islet_integrated, dims = 1:11)
+
+# Checking out the resolutions
+islet_integrated <- FindClusters(islet_integrated, resolution = c(0.05, 0.06, 0.07, 0.08))
+View(islet_integrated@meta.data)
+
+DimPlot(islet_integrated, group.by = "integrated_snn_res.0.05", label = TRUE)
+
+islet_integrated <- RunUMAP(islet_integrated, dims = 1:7, resolution = 0.05)
+
+#-------------------------Visualizing-----------------------------------------
+
+
+p1 <- DimPlot(islet_integrated, reduction = "umap", label = TRUE)
+p2 <- DimPlot(islet_integrated, reduction = 'umap', group.by = "patient_name")
+p3 <- DimPlot(islet_integrated, reduction = 'umap', group.by = "condition")
+
+grid.arrange(p1, p2, p3, ncol = 2, nrow = 2)
+
+combined_plot <- grid.arrange(p1, p2, p3, ncol = 2, nrow = 2)
+
+png("C://Users/jonan/Documents/1Work/scWork/Beta_Cell_Study/Figures/SecondSet/after_integration.png",
+    width = 2000, height = 1200)
+
+grid.draw(combined_plot)
+dev.off()
+
+DefaultAssay(islet_integrated)
+
+
+#----------------------------Finding INS---------------------------------------
+# Plotting INS expression on UMAP
+FeaturePlot(islet_integrated, features = "INS", min.cutoff = "q10", max.cutoff = "q90")
+
+
+#-------------------------------Identifying clusters -------------------------
+
+DefaultAssay(islet_integrated) <- "RNA"
+islet_integrated@meta.data$condition
+
+markers_cluster_1 <- FindConservedMarkers(islet_integrated,
+                                          ident.1 = 1,
+                                          grouping.var = 'condition')
+
+
+
+
+
+
+
+
